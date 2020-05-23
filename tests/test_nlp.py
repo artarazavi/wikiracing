e523cae429627b695c35b762c5a4ed6cb60d2c03
Arta Razavi <artarazavi@gmail.com>
2020-05-23 00:50:45 -0700

    moving stuff around so I can mock celery more effectively

--- tests/test_nlp.py
+++
@@ -1,8 +0,0 @@
-# from nlp.nlp import nlp_score
-
-# def test_nlp_score():
-#     return_data = nlp_score("Soup", "Hamburger")
-#     query = return_data[0]
-#     score = return_data[1]
-#     assert query == "Hamburger"
-#     assert score == 0.6055046166374669
